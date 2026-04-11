78b60c1f8f412ac8d09d31d7a3ce20df5700082b
Jitendra Meena <19606386+jitu-git@users.noreply.github.com>
2026-04-12 00:19:09 +0530

    docs update for widgets

--- content/publisher-widget/_meta.ts
+++ content/publisher-widget/_meta.ts
@@ -2,7 +2,6 @@ const meta = {
   "index": "Overview",
   "integration-guide": "Integration Guide",
   "init-token": "Generating Init Tokens",
-  "api-reference": "Widget API Reference",
 };
 
 export default meta;
